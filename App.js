305e06439024b0d55990f641c93df344f5901072
jonatan <villegasjoncena7@gmail.com>
2023-10-09 19:47:30 -0600

    webview

--- App.js
+++ App.js
@@ -1,9 +1,27 @@
 import React from "react";
-import Navigation from "./src/Navigation/Navigation";
+import { WebView } from 'react-native-webview';
+import { StyleSheet } from "react-native";
 
 export default function App() {
   return (
-    <Navigation/>
+    <WebView
+      style={styles.container}
+      originWhitelist={['*']}
+      source={{ uri: 'https://moonlit-gingersnap-47171f.netlify.app/' }}
+    />
   );
 }
-
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: '#ecf0f1',
+    padding: 8,
+  },
+  paragraph: {
+    margin: 24,
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+});
